b16de3163b946f965be4e524712caf333670e581
quexopacode <luismurcia0106@gmail.com>
2024-10-19 10:54:02 -0500

    Nuevo commit and pushMerge branch 'master' of https://github.com/quexopacode/Lab7-BD2

--- FormularioReserva.vb
+++ FormularioReserva.vb
@@ -3,32 +3,34 @@
 'Alejandro Mosquera
 'Luis Murcia
 Public Class FormularioReserva
+    Private Sub FormularioReserva_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dtpFechaEntrada.Value = DateTime.Now
+        dtpFechaSalida.Value = DateTime.Now.AddDays(1)
+    End Sub
     Private reserva As New Reserva()
     Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
         Try
-            ' Capturamos los valores del formulario
+            If String.IsNullOrEmpty(txtNombre.Text) Then
+                Throw New Exception("El nombre no puede estar vacío.")
+            End If
+            If dtpFechaSalida.Value <= dtpFechaEntrada.Value Then
+                Throw New Exception("La fecha de salida debe ser posterior a la fecha de entrada.")
+            End If
+            If cmbTipoHabitacion.SelectedIndex = -1 Then
+                Throw New Exception("Debe seleccionar un tipo de habitación.")
+            End If
+
             Dim nombre As String = txtNombre.Text
             Dim fechaEntrada As Date = dtpFechaEntrada.Value
             Dim fechaSalida As Date = dtpFechaSalida.Value
             Dim tipoHabitacion As String = cmbTipoHabitacion.SelectedItem.ToString()
 
-            ' Validaciones de entrada
-            If String.IsNullOrEmpty(nombre) Then
-                Throw New Exception("El nombre no puede estar vacío.")
-            End If
-            If fechaSalida <= fechaEntrada Then
-                Throw New Exception("La fecha de salida debe ser posterior a la fecha de entrada.")
-            End If
-            If String.IsNullOrEmpty(tipoHabitacion) Then
-                Throw New Exception("Debe seleccionar un tipo de habitación.")
-            End If
-
-            ' Llamamos a la capa lógica para insertar la reserva
             reserva.InsertarReserva(nombre, fechaEntrada, fechaSalida, tipoHabitacion)
-
-            ' Informamos al usuario que la reserva fue exitosa
             MessageBox.Show("Reserva agregada exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
-
+            txtNombre.Clear()
+            cmbTipoHabitacion.SelectedIndex = -1
+            dtpFechaEntrada.Value = DateTime.Now
+            dtpFechaSalida.Value = DateTime.Now.AddDays(1)
         Catch ex As Exception
             ' Mostrar el error al usuario en caso de fallo
             MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
@@ -39,4 +41,6 @@ Public Class FormularioReserva
         ReporteReservas.Show()
         Me.Hide()
     End Sub
+
+
 End Class
